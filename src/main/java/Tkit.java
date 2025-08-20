bd95313a3d1b6dd4a1e3d5c7e9baad391fab1cb3
Loh Xian Jie <e1138295@u.nus.edu>
2025-08-21 00:22:46 +0800

    Level-2-Add,-List

--- src/main/java/Tkit.java
+++ src/main/java/Tkit.java
@@ -1,30 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Tkit {
+
+    private enum Command {
+        ADD,
+        LIST,
+        BYE,
+        UNKNOWN
+    }
+
     public static void main(String[] args) {
         String identity = "not three kids in a trench coat";
         Scanner sc = new Scanner(System.in);
+
+        List<String> lst = new ArrayList<>();
+
         System.out.println("____________________ \n\nHello from  " + identity);
         System.out.println("What do you mean we are three kids in a trench coat?");
-        System.out.println("We are an adult person! \n ____________________\n");
+        System.out.println("We are an adult person! \n____________________\n");
 
-        while(true) {
+        while (true) {
             String input = sc.nextLine().trim();
-            
+
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________\n");
                 System.out.println("Goodbye fellow adult!");
                 System.out.println("____________________________________________________________");
                 break;
             }
-            
-            System.out.println("____________________________________________________________\n");
-            System.out.println("" + input + " heehee");
-            System.out.println("____________________________________________________________");
+
+            if (input.equals("list")) {
+                System.out.println("____________________________________________________________\n");
+                    if (lst.isEmpty()) {
+                        System.out.println("No entries yet.");
+                    } else {
+                        for (int i = 0; i < lst.size(); i++) {
+                            System.out.println((i + 1) + ". " + lst.get(i));
+                        }
+                    }
+                System.out.println("____________________________________________________________");
+            }
+
+            else{ 
+                System.out.println("____________________________________________________________\n");
+                System.out.println("added: " + input);
+                lst.add(input);
+                System.out.println("____________________________________________________________");
+            }
         }
 
         sc.close();
 
-        
     }
 }
